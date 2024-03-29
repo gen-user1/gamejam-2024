d800ac841e7de2902bb1ea35b264b9e57ca4f071
Aleksei Sohh <aleksei.sohh@ringit.ee>
2024-03-29 15:13:49 +0200

    Added submarine properties

--- Assets/Scripts/ScriptableObjects/SubmarineState.cs
+++ Assets/Scripts/ScriptableObjects/SubmarineState.cs
@@ -33,5 +33,47 @@ namespace ScriptableObjects
                 OnHealthChange?.Invoke(value);
             }
         }
+        
+        [SerializeField] private int depth = 100;
+
+        public Action<int> onDepthlChange;
+
+        public int Depth
+        {
+            get => depth;
+            set
+            {
+                depth = value;
+                onDepthlChange?.Invoke(value)
+            }
+        }
+
+        [SerializeField] private int safeDepthLevel = 100;
+
+        public Action<int> onSafeDepthLevelChange;
+
+        public int SafeDepthLevel
+        {
+            get => safeDepthLevel;
+            set
+            {
+                safeDepthLevel = value;
+                onSafeDepthLevelChange?.Invoke(value)
+            }
+        }
+        
+        [SerializeField] private int resistance = 100;
+
+        public Action<int> resistance;
+
+        public int Resistance
+        {
+            get => resistance;
+            set
+            {
+                resistance = value;
+                resistance?.Invoke(value)
+            }
+        }
     }
 }
