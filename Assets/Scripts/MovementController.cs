34a04c76320dd4cabbd64dc4b2de42fa89030c98
Alex P <evilstormhot@gmail.com>
2024-03-29 18:11:47 +0200

    Rotate submarine on movement

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,22 +7,23 @@ public class MovementController : MonoBehaviour
     [SerializeField] private Rigidbody rigidbodyComponent;
     [SerializeField] private new Camera camera;
     [SerializeField] private float speed = 3;
-    private bool downWasPressed;
-    private bool upWasPressed;
-    private bool leftWasPressed;
-    private bool rightWasPressed;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        var vertialAxis = Input.GetAxis("Vertical");
+        var horizontalAxis = Input.GetAxis("Horizontal");
+
         var cameraTransform = camera.transform;
         var cameraPosition = cameraTransform.position;
         cameraTransform.position = new Vector3(cameraPosition.x, rigidbodyComponent.position.y, cameraPosition.z);
+        RotateSubmarine(horizontalAxis);
     }
     
     private void FixedUpdate()
@@ -30,5 +31,21 @@ public class MovementController : MonoBehaviour
         var vertialAxis = Input.GetAxis("Vertical");
         var horizontalAxis = Input.GetAxis("Horizontal");
         rigidbodyComponent.velocity = new Vector3(horizontalAxis * speed, vertialAxis * speed, 0);
+    }
+
+    private void RotateSubmarine(float horizontalAxis) 
+    {
+        Vector3 submarineRotationVelocity = new Vector3(0, 0, 0);
+        if (horizontalAxis > 0 && rigidbodyComponent.rotation.y < 0.8) 
+        {
+            submarineRotationVelocity = new Vector3(0, 100, 0);
+        }
+        if (horizontalAxis < 0 && rigidbodyComponent.rotation.y > -0.8)
+        {
+            submarineRotationVelocity = new Vector3(0, -100, 0);
+        }
+
+        Quaternion deltaRotation = Quaternion.Euler(submarineRotationVelocity * Time.fixedDeltaTime);
+        rigidbodyComponent.MoveRotation(rigidbodyComponent.rotation * deltaRotation);
     }
 }
