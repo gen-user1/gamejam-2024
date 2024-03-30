654b7a08df2378b7223e8b9c043145182d0cade8
vash <pitkunait@gmail.com>
2024-03-30 11:43:30 +0200

    add manus

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -3,11 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    public static void MainMenu()
+    {
+        SceneManager.LoadSceneAsync("Scenes/MainMenu");
+    }
+
     public static void StartGame()
     {
         SceneManager.LoadSceneAsync("Scenes/SampleScene");
     }
-    
+
     public static void GameOver()
     {
         SceneManager.LoadSceneAsync("Scenes/GameOver");
