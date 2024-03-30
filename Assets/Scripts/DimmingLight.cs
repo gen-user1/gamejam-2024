b96fa9a452fde2173d54fd457c42e00f624d0600
vash <pitkunait@gmail.com>
2024-03-30 10:53:56 +0200

    fix more warnings

--- Assets/Scripts/DimmingLight.cs
+++ Assets/Scripts/DimmingLight.cs
@@ -5,25 +5,23 @@ public class DimmingLight : MonoBehaviour
 {
     public GameObject sub;
 
-    private new Light light;
+    private Light _light;
 
-    private new Camera camera;
+    private Camera _camera;
 
-    private Color originalColor = new Color(0.1921f, 0.3019f, 0.4745f);
+    private Color originalColor = new(0.1921f, 0.3019f, 0.4745f);
 
     private void Start()
     {
-        light = GetComponent<Light>();
-        camera = Camera.main;
+        _light = GetComponent<Light>();
+        _camera = Camera.main;
     }
 
     private void Update()
     {
         var position = sub.transform.position;
-        light.intensity = 1 - (Math.Abs(position.y) / 20);
+        _light.intensity = 1 - (Math.Abs(position.y) / 20);
         var colorStep = Math.Abs(position.y) / 20;
-        camera.backgroundColor = originalColor - new Color(0.1f * colorStep, 0.1f * colorStep, 0.1f * colorStep);
-        Debug.Log("Intencity " + light.intensity);
-        Debug.Log("Sub transform " + position.y);
+        _camera.backgroundColor = originalColor - new Color(0.1f * colorStep, 0.1f * colorStep, 0.1f * colorStep);
     }
 }
